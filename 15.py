ad52a68fe808799f27d334b2a20814fa327af5af
denis <molok33@yandex.ru>
2024-01-29 23:42:15 +0300

    Revert "meow"
    This reverts commit 7db6cc372bc91993f970015094e672aba1426750.

--- 15.py
+++
@@ -1,5 +0,0 @@
-def f(x, A):
-    return (x & 39 == 0) or ((x & 11 == 0) <= (x & A != 0))
-for a in range(1,1000):
-    if all(f(x, a) == 1 for x in range(1,1000)):
-        print(a)
--- 22.py
+++
@@ -1,8 +0,0 @@
-time = {'0': 0}
-
-for s in open('22.txt'):
-    s = s.replace(';', ' ')
-    s = s.split()
-    time[s[0]] = int(s[1]) + max(time[k] for k in s[2:])
-
-print(max(time.values()))
